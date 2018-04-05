4946f60d8f978df6bddf78c6a599e6bbbd1c40dc
Arnaud de_La_Chaise <arnaud.delachaise@gmail.com>
2018-04-05 17:59:49 +1000

    add JS working, without gentle animation though

--- app/javascript/components/navigation.js
+++ app/javascript/components/navigation.js
@@ -12,26 +12,29 @@
 
 function updateActiveSection() {
 	window.addEventListener('scroll', function(){
-		let sectionsToUpdate = document.querySelectorAll('.section-selector');
+		const sectionsToUpdate = document.querySelectorAll('.section-selector');
+		const sectionActiveMobile = document.getElementById("active-section-mobile");
 		for (let i = 0; i < sectionsToUpdate.length - 1; i++) {
-			let sectionText = sectionsToUpdate[i].innerHTML.toLowerCase().replace(" ","-");
+			let sectionText = sectionsToUpdate[i].innerText.toLowerCase().replace(" ","-");
 			let sectionY = document.getElementById(sectionText).getBoundingClientRect().top;
-			let nextSectionText = sectionsToUpdate[i+1].innerHTML.toLowerCase().replace(" ","-");
+			let nextSectionText = sectionsToUpdate[i+1].innerText.toLowerCase().replace(" ","-");
 			let nextSectionY = document.getElementById(nextSectionText).getBoundingClientRect().top;
 
 			if ( sectionY <= 1 && nextSectionY > 1) {
 				sectionsToUpdate[i].classList.add("active");
+				sectionActiveMobile.innerText = sectionsToUpdate[i].innerText
 			} else { sectionsToUpdate[i].classList.remove("active"); }
 		}
 		let lastSectionY = document.getElementById("contact").getBoundingClientRect().top;
 		if ( lastSectionY <= 1 ) {
 			sectionsToUpdate[sectionsToUpdate.length - 1].classList.add("active");
+			sectionActiveMobile.innerText = sectionsToUpdate[sectionsToUpdate.length - 1].innerText
 		} else { sectionsToUpdate[sectionsToUpdate.length - 1].classList.remove("active"); }
 	});
 }
 
 function listenToClick() {
-	const sections = document.querySelectorAll('.section-selector');
+	const sections = document.querySelectorAll(".section-selector, .section-selector-mobile");
 	for (let i = 0; i < sections.length; i++) {
     sections[i].addEventListener('click', function(){
     	let sectionToNavigateTo = sections[i].innerHTML;
