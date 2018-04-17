dd2c2a3ae3b4a338005b1bbb354feaf609638caf
Arnaud de_La_Chaise <arnaud.delachaise@gmail.com>
2018-04-17 19:26:25 +1000

    add basic AJAX, need to launch it automatically after page has been loaded

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,32 @@
 class PagesController < ApplicationController
   def home
   	@active_tab = "HOME"
-  	cache = ActiveSupport::Cache.lookup_store(:file_store, '/tmp/cache')
+  	@cache = ActiveSupport::Cache.lookup_store(:file_store, '/tmp/cache')
+    load_urls
+  end
+
+  def website
+    @name = params[:name]
+    load_urls
+    @urls.each { |url| @url = url[1] if url[0] == @name }
+    respond_to do |format|
+        format.html
+        format.js
+    end
+  end
+
+  private
+
+  def load_urls
+    @urls = [
+      # EXPERIENCE
+      ["woolworths", "https://www.woolworthsgroup.com.au"],
+      ["pernodricard", "https://havana-club.com/en-ww/"],
+      ["schlumberger", "https://www.slb.com/"],
+      # EDUCATION
+      ["polytechnique", "http://www.polytechnique.edu/en"],
+      ["lse", "http://www.lse.ac.uk/"],
+      ["jbsay", "http://lyc-jb-say.scola.ac-paris.fr/"]
+    ]
   end
 end
