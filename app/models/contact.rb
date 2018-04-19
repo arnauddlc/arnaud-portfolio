63b298226c19b54255db972475958ae2ecd4f87a
Arnaud de_La_Chaise <arnaud.delachaise@gmail.com>
2018-04-20 08:45:10 +1000

    add basic mailer, now for some AJAX

---
+++ app/models/contact.rb
@@ -0,0 +1,16 @@
+class Contact < MailForm::Base
+  append :remote_ip, :user_agent
+  attribute :name,      validate: true
+  attribute :email,     validate: /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
+  attribute :subject,   validate: true
+  attribute :message
+  validates :message,   length: { in: 30..5000 }
+
+  def headers
+    {
+      subject: "AdLC - #{subject}",
+      to: 'arnaud.delachaise@gmail.com',
+      from: %("#{name}" <#{email}>)
+    }
+  end
+end
