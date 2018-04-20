69ac8259d2241b83a0a55db889713e2e9ec03d5c
Arnaud de_La_Chaise <arnaud.delachaise@gmail.com>
2018-04-20 11:34:29 +1000

    change css of flashes and solve apparition of both alert and notice at the same tinme

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -7,13 +7,15 @@ class ContactsController < ApplicationController
   	@contact = Contact.new(params[:contact])
     @contact.request = request
     if @contact.deliver
+    	flash[:alert] = nil
       flash[:notice] = "Your message \'#{@contact.subject}\' has been sent. I will be in touch soon!"
       respond_to do |format|
       	format.html
       	format.js
       end
     else
-      flash[:alert] = "Cannot send message \'#{@contact.subject}\'"
+      flash[:alert] = "Cannot send message#{"' " + @contact.subject + "'" unless @contact.subject == "" }"
+      flash[:notice] = nil
       respond_to do |format|
       	format.html
       	format.js
