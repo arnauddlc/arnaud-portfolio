e09e2a2c33bc5b4c3fba5aeb5b1d42bd6fc44872
Arnaud de_La_Chaise <arnaud.delachaise@gmail.com>
2018-04-20 14:37:29 +1000

    add some styling

--- app/javascript/components/loading_website.js
+++ app/javascript/components/loading_website.js
@@ -2,13 +2,19 @@ function indicateLoading() {
 	const buttons = document.querySelectorAll(".loading");
 	buttons.forEach(function(button){
 		button.addEventListener("click", function(e){
+			button.classList.add("loading-wait");
 			button.innerText = "Loading...";
+			var websiteCard = button.closest(".card-website");
+			websiteCard.classList.add("loading-wait");
 
 			if (button.id == "load-all"){
 				button.classList.remove("btn-primary");
 				button.classList.add("btn-secondary");
 				buttons.forEach(function(buttonA){
+					buttonA.classList.add("loading-wait");
 					buttonA.innerText = "Loading...";
+					var websiteCard = buttonA.closest(".card-website");
+					websiteCard.classList.add("loading-wait");
 				})
 			};
 		})
