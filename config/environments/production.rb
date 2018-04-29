f1e980e8a0d869081af1b5db0e4bfbc3f6f289fb
Arnaud de_La_Chaise <arnaud.delachaise@gmail.com>
2018-04-29 18:59:57 +1000

    add figaro file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,6 +79,12 @@ Rails.application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
+  config.action_mailer.delivery_method = :mailgun
+  config.action_mailer.mailgun_settings = {
+   api_key: 'ENV["mailgun_secret_api_key"]',
+   # domain: 'mydomain.com',
+  }
+
   # Use a different logger for distributed setups.
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
