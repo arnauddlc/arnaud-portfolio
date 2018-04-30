9d224f34402a6cfd1bdc39f3944c54e6694fd1cf
Arnaud de_La_Chaise <arnaud.delachaise@gmail.com>
2018-04-30 10:31:27 +1000

    try mail setting

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,13 +67,13 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    :address              => "smtp.gmail.com",
-    :port                 => 587,
-    :domain               => "gmail.com",
-    :user_name            => ENV['g_account'],
-    :password             => ENV['g_pass'],
-    :authentication       => "plain",
-    :enable_starttls_auto => true
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "gmail.com",
+    user_name: ENV['g_account'],
+    password: ENV['g_pass'],
+    authentication: "plain",
+    enable_starttls_auto: true
   }
 
 
